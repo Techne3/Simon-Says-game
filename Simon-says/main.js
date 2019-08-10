22331deeae49e13acf2ec2374c220397888c7bd4
Timothy Sharkey <Techne3@gmail.com>
2019-08-10 00:24:58 -0600

    Add files via upload

---
+++ Simon-says/main.js
@@ -0,0 +1,237 @@
+let order = [];
+let playerOrder = [];
+let flash;
+let turn;
+let good;
+let compTurn;
+let intervalID;
+let strict = false;
+let noise = true;
+let on = false;
+let win; 
+
+const turnCounter= document.querySelector('#turn');
+const topLeft = document.querySelector('#topleft');
+const topRight = document.querySelector('#topright');
+const bottomLeft = document.querySelector('#bottomleft');
+const bottomRight = document.querySelector('#bottomright');
+const strictButton = document.querySelector('#strict');
+const onButton = document.querySelector('#on');
+const startButton = document.querySelector('#start');
+
+
+strictButton.addEventListener('change',(event)=>{
+    // console.log("checked")
+    if(strictButton.checked == true) {
+        strict = true;
+    }else {
+        strict = false;
+    }
+});
+
+onButton.addEventListener('click',(event)=>{
+    if(onButton.checked == true){
+        on= true;
+        turnCounter.innerHTML = "-";
+    }else{
+        on = false;
+        turnCounter.innerHTML= '';
+        clearColor();
+        clearInterval(intervalID);
+    }
+});
+
+startButton.addEventListener('click',(e)=>{
+    if(on || win){
+        play();
+    }
+});
+
+// play function
+
+function play(){
+    win = false;
+    order = [];
+    playerOrder = [];
+    flash = 0;
+    intervalID = 0;
+    turn = 1;
+    turnCounter.innerHTML=1;
+    good = true;
+    for (var i=0;i<20;i++) { 
+        // gets random number 1-4
+        order.push(Math.floor(Math.random() * 4) + 1);
+    }
+    // console.log(order);
+    compTurn = true;
+    // start fist turn
+    // setInterval run a function after every 800 milliseconds
+    // it will keep repeating till interval is cleared
+    intervalID = setInterval(gameTurn,800);
+}
+
+
+// turn function
+function gameTurn() {
+    on = false;
+  
+    if (flash == turn) {
+      clearInterval(intervalID);
+      compTurn = false;
+      clearColor();
+      on = true;
+    }
+
+    if (compTurn) {
+        clearColor();
+        setTimeout(() => {
+          if (order[flash] == 1) one();
+          if (order[flash] == 2) two();
+          if (order[flash] == 3) three();
+          if (order[flash] == 4) four();
+          flash++;
+        }, 200);
+      }
+    }
+
+    function one() {
+        if (noise) {
+          let audio = document.getElementById("clip1");
+          audio.play();
+        }
+        noise = true;
+        topLeft.style.backgroundColor = "lightgreen";
+      }
+      
+      function two() {
+        if (noise) {
+          let audio = document.getElementById("clip2");
+          audio.play();
+        }
+        noise = true;
+        topRight.style.backgroundColor = "tomato";
+      }
+      
+      function three() {
+        if (noise) {
+          let audio = document.getElementById("clip3");
+          audio.play();
+        }
+        noise = true;
+        bottomLeft.style.backgroundColor = "yellow";
+      }
+      
+      function four() {
+        if (noise) {
+          let audio = document.getElementById("clip4");
+          audio.play();
+        }
+        noise = true;
+        bottomRight.style.backgroundColor = "lightskyblue";
+      }
+
+  function clearColor() {
+    topLeft.style.backgroundColor = "darkgreen";
+    topRight.style.backgroundColor = "darkred";
+    bottomLeft.style.backgroundColor = "goldenrod";
+    bottomRight.style.backgroundColor = "darkblue";
+  }
+  
+  function flashColor() {
+    topLeft.style.backgroundColor = "lightgreen";
+    topRight.style.backgroundColor = "tomato";
+    bottomLeft.style.backgroundColor = "yellow";
+    bottomRight.style.backgroundColor = "lightskyblue";
+  }
+  
+  topLeft.addEventListener('click',(event)=>{
+      if(on) {
+          playerOrder.push(1);
+          check();
+          one();
+          if(!win){
+              setTimeout(()=>{
+                  clearColor();
+              },300);
+          }
+      }
+  });
+  topRight.addEventListener('click',(event)=>{
+      if(on) {
+          playerOrder.push(2);
+          check();
+          two();
+          if(!win){
+              setTimeout(()=>{
+                  clearColor();
+              },300);
+          }
+      }
+  });
+  bottomLeft.addEventListener('click',(event)=>{
+      if(on) {
+          playerOrder.push(3);
+          check();
+          three();
+          if(!win){
+              setTimeout(()=>{
+                  clearColor();
+              },300);
+          }
+      }
+  });
+  bottomRight.addEventListener('click',(event)=>{
+      if(on) {
+          playerOrder.push(4);
+          check();
+          four();
+          if(!win){
+              setTimeout(()=>{
+                  clearColor();
+              },300);
+          }
+      }
+  });
+
+
+function check() {
+    if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1])
+    good = false;
+    if(playerOrder.length == 20 && good){
+        winGame();
+    }
+    if (good == false){
+        flashColor();
+        turnCounter.innerHTML = "NO!";
+        setTimeout(()=>{
+            turnCounter.innerHTML ="turn";
+            clearColor();
+            if (strict){
+                play();
+            }else {
+               compTurn = true;
+               flash = 0;
+               playerOrder = [];
+               good = true;
+               intervalID = setInterval(gameTurn,800);
+            }    
+        },800);
+        noise = false;
+    }
+    if (turn == playerOrder.length && good && !win){
+        turn++;
+        playerOrder = [];
+        compTurn= true;
+        flash = 0 ;
+        turnCounter.innerHTML= turn;
+        intervalID = setInterval(gameTurn,800)
+    }
+}
+  
+
+function winGame () {
+    flashColor();
+    turnCounter.innerHTML="WIN!";
+    on = false;
+    win = true;
+}
